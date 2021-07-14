67e7cf1e5ff8ef7a4eeb82cf6965268024364c9d
Farkasvölgyi <isty001@gmail.com>
2021-07-14 20:31:26 +0200

    each componenet with own window

--- lib/argyle/view/text.rb
+++ lib/argyle/view/text.rb
@@ -4,11 +4,28 @@ class Argyle::View::Text < Argyle::View::Base
   # @param _ [Argyle::Component::Context]
   #
   def render(window, component, _)
-    x, y, width, height = component_gemoetry(window, component)
+
+    unless component.fired_up?
+      x, y, width, height = component_gemoetry(window, component)
+
+      component.fire_up(window.subwin(height, width, y, x))
+    end
+
+    display_text(component)
+  end
+
+  private
+
+  # @param component [Argyle::Component::Text]
+  #
+  def display_text(component)
+    y = 0
+
+    window = component.window
 
     style(window, component.style) do
-      wrap_text(component.value, width, height).each do |line|
-        window.setpos(y, x)
+      wrap_text(component.value, window.maxx, window.maxy).each do |line|
+        window.setpos(y, 0)
         window.addstr(line)
         y += 1
       end
